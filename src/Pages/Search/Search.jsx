a0818777a8f8596102c64e4d294c5a3d5a257b30
Drastic_Coder <deepbansodeofficial4205@gmail.com>
2022-10-12 13:26:10 +0530

    Add search section

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React,{useState , useEffect} from 'react';
+import axios from 'axios';
+
+import Content from '../../components/SingleContent/Content';
 
 const Search = () => {
+
+  const [searchText, setSearchText] = useState('');
+  const [content, setContent] = useState([]);
+
+  const fetchTrending = async () => {
+    const { data } = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=a87622cd39e327898dda6b34753ad69d&language=en-US&query=${searchText!='' ? searchText : 'spiderman' }&page=1&include_adult=false`)
+    setContent(data.results);
+  }
+
+  useEffect(() => {
+    fetchTrending();
+  }, [])
+
+
+
+
   return (
-    <div>Search</div>
+    <>
+      <div className='text-white font-bold pl-8 pt-3 text-2xl'>Search</div>
+    
+    <div className='bg-purple-200 mx-5 p-5 '>
+      <input type="text" placeholder='Search here...' value={searchText} onChange={(e) => { setSearchText(e.target.value); fetchTrending() }} className='appearance-none  border-none w-4/5 text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none' />
+
+    </div>
+
+      
+
+    {/* <button onClick={fetchTrending}>Search</button> */}
+
+    <div className='flex flex-wrap justify-around mb-20'>
+      {content.length!=0 ? content.map((c) => (
+        <Content key={c.id} id={c.id} poster={c.poster_path} title={c.title || c.name} date={c.first_air_date || c.release_date} media_type={c.media_type} vote_average={c.vote_average} />
+      )) : <h1>No result Found</h1>}
+      
+    </div>
+
+    </>
   )
 }
 
--- src/components/SingleContent/Content.jsx
+++ src/components/SingleContent/Content.jsx
@@ -15,7 +15,7 @@ const Content = ({
     <div className='relative bg-gradient-to-r from-[#141e30] to-[#764AF1]  text-white hover:bg-white rounded-lg  m-4 p-1 rounded-b-[20px]'>
 
       
-        <span class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
+        <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
           {vote_average.toString().length === 1 ? vote_average + '.0' : vote_average.toString().slice(0, 3)}
           </span>
 
